a2455f304bec6094379eecf490791e42a1008b60
PanhaLtd <panhaltb@gmail.com>
2020-11-15 11:55:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h1>Hello Cambodia!</h1>
+    </div>
+  );
+}
+
+export default App;
