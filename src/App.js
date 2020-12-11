eea174b0a57c0d54471acf7bebf9423f1ffbd868
Panha Long <73827466+PanhaLtd@users.noreply.github.com>
2020-12-12 01:58:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <h1>Hello Cambodia Hrd! Ouhsa</h1>
+      <h1>Hello Cambodia Hrd!</h1>
     </div>
   );
 }
